e7829e9ea0d775599551f28956b31691dbb5f3e6
RyanCortino <RyanCortino@gmail.com>
2020-03-09 19:42:44 -0500

    Added base Unit Controller
    Implemented Unit Controllers and a simple input processor.

---
+++ Assets/Scripts/Controllers/UnitController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace aberrantGaming.Breakout
+{
+    public class UnitController : UnitMotor
+    {
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawRay(this.transform.position, this.transform.forward);
+        }
+    }
+}
